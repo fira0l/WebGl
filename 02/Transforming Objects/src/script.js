e0c34178a4e86097115724a1de6db5fcb274cb28
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-12-26 02:27:51 -0800

    Commit 1.4 Transforming Objects using Postion scale and Rotation and group to manage the together

--- 02/Transforming Objects/src/script.js
+++ 02/Transforming Objects/src/script.js
@@ -9,10 +9,38 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+cube2.position.x = -1.5
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+cube3.position.x = 1.5
+group.add(cube3)
+
+// Axes Helper
+const axeshelper = new THREE.AxesHelper()
+scene.add(axeshelper)
+
 
 /**
  * Sizes
@@ -27,7 +55,13 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+
+
 scene.add(camera)
+
+// camera.lookAt(mesh.position)
+
+// console.log(mesh.position.distanceTo(camera.position))
 
 /**
  * Renderer
