85c97881cb4be72f20111e4c81a2129b4f2d93b5
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-12-28 05:12:03 -0800

    Commit 1.13 Rendering the object with the given canvas device ratio

--- 02/Fullscreen and Resizing/src/script.js
+++ 02/Fullscreen and Resizing/src/script.js
@@ -58,6 +58,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
