c973d052572c219f1936e8f28d6bbc560b2d206f
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-12-29 10:32:45 -0800

    Commit 1.22 Adding Debugging UI to the page

--- 02/Textures/src/script.js
+++ 02/Textures/src/script.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
+import * as dat from 'lil-gui'
 
 /**
  * Base
@@ -10,13 +12,25 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+const gui = new dat.GUI()
+
+const parameters = {
+    color: 0xff0000,
+}
+
 /**
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+gui.addColor(material,'color')
+gui.add(mesh.position,'y').name('elevation').min(-3).max(3).step(0.01)
+gui.add(mesh.position,'x').name('x-distance').min(-3).max(3).step(0.01)
+gui.add(mesh.position,'z').name('closeness').min(-3).max(3).step(0.01)
+gui.add(material,'wireframe')
 
 /**
  * Sizes
